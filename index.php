82630e1597ccf3485cca9888e27b72399083d2b1
Marco Aurélio <macarena@maloca.(none)>
2013-03-07 20:21:58 -0300

    add bootstrap

--- index.php
+++ index.php
@@ -6,8 +6,26 @@ require_once("functions.php");
 
 $lista = listaPaginas();
 
+?>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+    <h1>Hello, world!</h1>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+<?
 foreach($lista as $pagina) {
 	echo "<a href=page.php?id=$pagina[id]>$pagina[titulo]</a><br />";
 }
 
 ?>
+</body>
+</html>
